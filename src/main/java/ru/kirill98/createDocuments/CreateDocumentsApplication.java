3f13d36716db50a296cc0e781aa7af9b0c07b495
Kirill <k.lubarskih@mail.ru>
2022-11-06 01:48:08 +0400

    Initial commit

---
+++ src/main/java/ru/kirill98/createDocuments/CreateDocumentsApplication.java
@@ -0,0 +1,13 @@
+package ru.kirill98.createDocuments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateDocumentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateDocumentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kirill98/createDocuments/CreateDocumentsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kirill98.createDocuments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateDocumentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
